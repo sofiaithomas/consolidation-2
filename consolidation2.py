12c0b73fe07895dcba80d5fa983c17427783ab17
Sofia Thomas <sofia.i.thomas@gmail.com>
2024-05-01 22:01:01 -0400

    created max score var, print line for welcome, 3 variables for 3 dice that randomly choose from 1-6

--- consolidation2.py
+++ consolidation2.py
@@ -1,3 +1,14 @@
 # Tuple out Game
 import random
+end_score = 50
 
+print("Welcome to the 'Tuple Out' dice game! ")
+
+#rolls = 3
+#for i in range(0,rolls):
+die_1 = random.randint(1,6)
+die_2 = random.randint(1,6)
+die_3 = random.randint(1,6)
+
+score = die_1 + die_2 + die_3
+print(score)
