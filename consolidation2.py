f2a54d6894d8a77897dfa2046f75c14ddd20a5f0
Sofia Thomas <sofia.i.thomas@gmail.com>
2024-05-08 18:57:18 -0400

    Finally achieved a working elif statement for fixed die and allowing the user to reroll the unfixed die. Also rearranged some previous lines for more efficiancy.

--- consolidation2.py
+++ consolidation2.py
@@ -32,24 +32,39 @@ def play_game():
             dice = roll_dice()
             print("These are your rolled die: ", dice)
             # up to here if runs infinitely displaying your rolled dice, now need to update the score so it doesn't run forever
+            player_score += sum(dice)
 
             # if statement for tupling
             if dice[0] == dice[1] == dice[2]:
                 print("Uh oh you have tupled! You get 0 points to your score this round")
                 player_score = 0
+                #print(f"You have a score of: {score}")
+                #print("------------------------------")
                 break
 
-            if dice[0] == dice[1] or dice[1] == dice[2] or dice[2] == dice[0]:
+            elif dice[0] == dice[1] or dice[1] == dice[2] or dice[2] == dice[0]:
                 print("The two dice with the same number are fixed")
-                reroll_choice = input("Do you want to re-roll the non-fixed die")
+                reroll_choice = input("Do you want to reroll the unfixed die? y or n: ")
+                if reroll_choice == 'y':
+                    reroll_die = int(input("Which die in the set is being re-rolled: 1, 2, or 3"))
+                    dice[reroll_die - 1] = random.randint(1,6)
+                    print("Your dice with the new re-rolled:", dice)
+
+                    if dice[0] == dice[1] == dice[2]:
+                        print("Uh oh you have tupled! You get 0 points to your score this round")
+                        player_score + 0
+                        break
+                else:
+                    print(f"Your score is: {score + player_score}")
+                    print("----------------------------")
+                    break
+
             
-            choice = input("Do you want to reroll any dice? (y or n): ").lower()
-            if choice != 'y':
-                player_score += sum(dice)
-                break
+            print(f"Your score is: {score + player_score}")
+            print("----------------------------")
+        
         score += player_score
-        print(f"You have a score of: {score}")
-        print(".")
+                        
     print(f"Yay! You got over 50 points with a score of {score}")
 
 
